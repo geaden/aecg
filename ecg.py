fix: fix M parameter calculation

ecg.py
@@ -40,8 +40,8 @@ class ECG(Algorithm, MComputeMixin):
         eo: ErroneousOracle,
         lmo: LinearMinimizationOracle,
         epsilon: float,
-        M: float = 1.0,
-        R: float = 1.0,
+        M: float,
+        R: float,
         boundedness: Boundedness = Boundedness.UNBOUNDED,
         keep_history=False,
     ):
@@ -103,8 +103,8 @@ class ECG(Algorithm, MComputeMixin):
             w_next = w + eta_t * p
             log(f"{w_next=}")
 
+            self.compute_M(self._objective.gradient(w))
             L_t = max(self._compute_Lt(t, w, w_next, g_hat, p), np.longlong(0))
-
             self._Lt.append(L_t)
 
             self._track_history(w_next)
@@ -129,9 +129,8 @@ class ECG(Algorithm, MComputeMixin):
             $L_t$.
         """
         j = t + 2
-        M = self.compute_M(g_hat)
         inexact_part = (
-            np.dot(g_hat, p) + self._boundedness * self._epsilon * M * self._R
+            np.dot(g_hat, p) + self._boundedness * self._epsilon * self._M * self._R
         )
         numerator = j * (
             j * (self._objective(w_next) - self._objective(w)) - 2 * inexact_part
@@ -155,12 +154,15 @@ class ECG(Algorithm, MComputeMixin):
 
         self._delta = [dt]
         for t in range(self._max_iterations):
-            w, _ = self._history[t]
-            g_hat = self._eo(self._objective.gradient, w)
-            M = self.compute_M(g_hat)
+            self.compute_M(self._history[t][0])
             delta_t = (
                 t / (t + 2) * self._delta[t]
-                + 2 * (1 + self._boundedness) * self._epsilon * M * self._R / (t + 2)
+                + 2
+                * (1 + self._boundedness)
+                * self._epsilon
+                * self._M
+                * self._R
+                / (t + 2)
                 + self._Lt[t] * self._R**2 / (t + 2) ** 2
             )
 

helper.py
@@ -234,19 +234,20 @@ class MComputeMixin:
     Mixing for computing $M$ value.
     """
 
-    def compute_M(self, g_hat: np.ndarray) -> np.float64:
+    _M: np.longdouble
+
+    def __init__(self):
+        self._M = np.longdouble(0)
+
+    def compute_M(self, gradient: np.ndarray):
         """
         Computes $M$ from the given inexact gradient value.
 
         Args:
-          g_hat: inexact gradient value
-
-        Returns:
-            computed value of $M$
+          gradient: gradient value
         """
         if _COMPUTE_M:
-            return np.linalg.norm(g_hat)
-        return self._M
+            self._M = np.linalg.norm(gradient)
 
 
 class SmoothnessStepSizeStrategy(StepSizeStrategy, MComputeMixin):
@@ -277,8 +278,7 @@ class SmoothnessStepSizeStrategy(StepSizeStrategy, MComputeMixin):
         self._boundedness = _boundedness
 
     def __call__(self, g_hat: np.ndarray, p: np.ndarray) -> float:
-        M = self.compute_M(g_hat)
-        numerator = np.dot(g_hat, p) + self._boundedness * self._epsilon * M * self._R
+        numerator = self._compute_numerator(g_hat, p)
         denominator = np.multiply(
             self._L_t, np.linalg.norm(p) ** 2, dtype=np.longdouble
         )
@@ -291,16 +291,16 @@ class SmoothnessStepSizeStrategy(StepSizeStrategy, MComputeMixin):
         g_hat: np.ndarray,
         p: np.ndarray,
     ) -> bool:
-        M = self.compute_M(g_hat)
-        numerator = np.pow(
-            self._boundedness * self._epsilon * M * self._R + np.dot(g_hat, p), 2
-        )
+        numerator = np.pow(self._compute_numerator(g_hat, p), 2)
         denominator = np.multiply(
             np.multiply(2, self._L_t, dtype=np.longdouble),
             np.pow(np.linalg.norm(p), 2, dtype=np.longdouble),
             dtype=np.longdouble,
         )
         return f - f_next >= numerator / denominator
+
+    def _compute_numerator(self, g_hat: np.ndarray, p: np.ndarray) -> np.longdouble:
+        return self._boundedness * self._epsilon * self._M * self._R + np.dot(g_hat, p)
 
 
 class ErroneousOracle(ABC):
@@ -352,11 +352,12 @@ class CWEOracle(ErroneousOracle):
         self._ensure_relative_error_bounds()
         true_gradient = gradient(x)
         erroneous_gradient = np.zeros_like(true_gradient)
-        for i in range(len(true_gradient)):
-            gradient_i = true_gradient[i]
-            error = np.random.uniform(1 - self.epsilon, 1 + self.epsilon)
-            # Relative error with sign preservation
-            erroneous_gradient[i] = np.sign(gradient_i) * abs(gradient_i) * error
+        error = (
+            np.random.uniform(-1, 1, size=x.shape)
+            * self.epsilon
+            * np.abs(true_gradient)
+        )
+        erroneous_gradient = true_gradient + error
 
         check(
             self._is_valid(true_gradient, erroneous_gradient),